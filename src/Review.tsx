9ff9b4097105b58802cb6a36d73c004e4160940b
DevCoup® <su@devcoup.com>
2023-01-31 10:50:30 -0500

    Style fixes

--- src/Review.tsx
+++ src/Review.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { useContestants, useReviews, useSubmission } from "./StoreContext";
+import {
+  useReviews,
+  useSelectedContestant,
+  useSubmission,
+} from "./StoreContext";
+import useWindowDimensions from "./useWindowDimensions";
 
 const ReviewPanel = () => {
   const { reviews } = useReviews();
   const { submission } = useSubmission();
-  const { contestants } = useContestants();
-  const selected = contestants.find(
-    (contestant) => contestant.id === submission
-  ) || { name: "Selected Contestant" };
+  const { selectedContestant: selected } = useSelectedContestant();
   const selectedReviews = reviews.filter((review) => review.for === submission);
+  const styles = useStyles();
   return (
-    <div style={{ ...(styles.container as any) }}>
+    <div style={styles.container}>
       <h2>Reviews for {selected.name}</h2>
-      <div style={styles.reviews as any}>
+      <div style={styles.reviews}>
         {selectedReviews.map((review) => (
-          <div key={review.id}>
+          <div key={review.id} style={styles.review}>
             <h3>{review.name}</h3>
-            <div dangerouslySetInnerHTML={{ __html: review.review }} />
+            <h4>Score: {review.rating} / 100</h4>
+            <div style={styles.reviewText} dangerouslySetInnerHTML={{ __html: review.review }} />
           </div>
         ))}
       </div>
@@ -24,30 +28,48 @@ const ReviewPanel = () => {
   );
 };
 
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    padding: 20,
-    backgroundColor: "lightblue",
-    border: "1px solid black",
-    borderRadius: 5,
-    flexGrow: 1,
-    height: "40%",
-    margin: 10,
-    overflow: "scroll",
-  },
-  reviews: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    marginLeft: 20,
-    padding: 20,
-    overflow: "scroll",
-  },
+const useStyles = () => {
+  const { width, height } = useWindowDimensions();
+  const portrait = height > width;
+  return {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      padding: 20,
+      backgroundColor: "lightblue",
+      border: "1px solid black",
+      borderRadius: 5,
+      flexGrow: 1,
+      height: "40%",
+      margin: 10,
+      overflow: "scroll",
+    } as any,
+    reviews: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      marginLeft: -20,
+      whiteSpace: portrait ? "break-spaces" : "normal",
+    } as any,
+    review: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      marginLeft: 20,
+    } as any,
+    reviewText: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        whiteSpace: "break-spaces",
+        maxWidth: width - 40,
+    } as any,
+  };
 };
 
 export default ReviewPanel;
--- src/Submission.tsx
+++ src/Submission.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useContestants, useSubmission } from "./StoreContext";
+import React, { useEffect, useRef } from "react";
+import { useSelectedContestant } from "./StoreContext";
 import useWindowDimensions from "./useWindowDimensions";
 
 const Submsission = () => {
-  const { contestants } = useContestants();
-  const { submission } = useSubmission();
-  const selected = contestants.find(
-    (contestant) => contestant.id === submission
-  ) || { url: "", name: "" };
+  const { selectedContestant: selected } = useSelectedContestant();
   const { width, height } = useWindowDimensions();
+  const [viewSource, setViewSource] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const iframe = useRef<HTMLIFrameElement>(null);
   const iframeSrc = useRef<HTMLTextAreaElement>(null);
   useEffect(() => {
@@ -16,33 +14,64 @@ const Submsission = () => {
       if (selected.url && iframe.current && iframeSrc.current) {
         if (iframe.current.src !== selected.url) {
           iframe.current.src = selected.url;
-          iframe.current.onload = () => {
-            // if (iframe.current && iframe.current.contentDocument) {
-            if (iframeSrc.current) {
-              iframeSrc.current.value =
-                iframe.current?.contentDocument?.documentElement.outerHTML ||
-                "";
-            }
-            // }
-          };
+          setLoading(true);
+          iframe.current.onload = () => setLoading(false);
+          iframe.current.onerror = () => setLoading(false);
+          iframe.current.onabort = () => setLoading(false);
         }
       }
     }
     load();
   }, [selected.url]);
 
+  useEffect(() => {
+    if (!loading) {
+      if (iframe.current && iframeSrc.current) {
+        try {
+          iframeSrc.current.value =
+            (iframe.current.contentWindow?.document.body.parentNode as any)?.outerHTML ||
+            "Error...";
+        } catch (e) {
+          iframeSrc.current.value = "Error...";
+        }
+      }
+    }
+    console.log("loading", loading, iframe.current, iframeSrc.current);
+  }, [iframe, loading]);
+
   const portrait = height > width;
 
   return (
     <div>
       <pre
-        style={{ overflow: "hidden", marginLeft: 20, fontSize: 18 }}
+        style={{ overflow: "scroll", marginLeft: 20, fontSize: 18, scrollbarWidth: "none" }}
       >{`${selected.name} - ${selected.url}`}</pre>
+      {/* <button onClick={() => setViewSource(!viewSource)}>
+        View {viewSource ? "Result" : "Source"}
+      </button> */}
+      {loading && <div>Loading...</div>}
+      <textarea
+        ref={iframeSrc}
+        style={{
+          overflow: "scroll",
+          display: viewSource ? "block" : "none",
+          marginLeft: 20,
+          fontSize: 18,
+          float: portrait ? "none" : "left",
+          margin: portrait ? "auto" : 20,
+          width: portrait ? "100%" : width * 0.8 - 40,
+        }}
+      />
       <iframe
         ref={iframe}
-        style={{ float: portrait ? "none" : "left", margin: portrait ? "auto" : 20 }}
+        style={{
+          float: portrait ? "none" : "left",
+          margin: portrait ? "auto" : 20,
+          width: portrait ? "100%" : width * 0.8 - 40,
+          display: viewSource ? "none" : "block",
+        }}
         src={selected.url.replace("http://", "//")}
-        width={width}
+        width={"100%"}
         height={height * 0.5}
         title="submission"
       />
